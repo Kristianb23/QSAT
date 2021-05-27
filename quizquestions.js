67a7c9d4a6cb8e285599df1aa42a608512662a72
Kristianb23 <76174180+Kristianb23@users.noreply.github.com>
2021-05-27 02:11:14 +0200

    quiz

---
+++ quizquestions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "when is tomorrow?",
+    answer: "tomorrow",
+    options: [
+      "tomorrow",
+      "today",
+      "today",
+      "today"
+    ]
+  },
+    {
+    numb: 2,
+    question: "when is tomorrow?",
+    answer: "tomorrow",
+    options: [
+      "tomorrow",
+      "today",
+      "today",
+      "today"
+    ]
+  },
+    {
+    numb: 3,
+    question: "when is tomorrow?",
+    answer: "tomorrow",
+    options: [
+      "tomorrow",
+      "today",
+      "today",
+      "today"
+    ]
+  },
+    {
+    numb: 4,
+    question: "when is tomorrow?",
+    answer: "tomorrow",
+    options: [
+      "tomorrow",
+      "today",
+      "today",
+      "today"
+    ]
+  },
+    {
+    numb: 5,
+    question: "when is tomorrow?",
+    answer: "tomorrow",
+    options: [
+      "tomorrow",
+      "today",
+      "today",
+      "today"
+    ]
+  },
+
+];
